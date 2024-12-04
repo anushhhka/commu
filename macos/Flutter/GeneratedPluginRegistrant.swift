8c25e34826a8edc329dbf24dc2a6ed5f95dcd3a5
Khuzaima <khuzaimaansari2004@gmail.com>
2024-12-04 19:21:06 +0530

    Downgraded Flutter

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import firebase_auth
 import firebase_core
 import firebase_messaging
 import path_provider_foundation
-import sqflite_darwin
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
